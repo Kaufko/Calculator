c5b9096fa7fc2e5131602de335f16b44ffc336e7
Kaufko <foxyczytcz@gmail.com>
2024-02-29 15:05:57 +0100

    Added Functions.cs and moved all methods to it. Added test Call to program.cs.

---
+++ Calculator/Functions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public static class Operators
+    {
+        public static float Pow(float number, int powerOf)
+        {
+            float result = number;
+            for (int i = 0; i < powerOf; i++)
+            {
+                result *= number;
+            }
+            return result;
+        }
+
+        public static float Sqrt(float number, int sqrtNum)
+        {
+            float result = number;
+            result *= 1.0f / sqrtNum;
+            return result;
+        }
+        
+    }
+
+    public static class Miscellaneous
+    {
+        public static void WriteToHistory(string operation, float result)
+        {
+            File.AppendAllText($"{Path.GetTempPath()}history.log", $"{operation} {result}");
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,36 +1,13 @@
 ﻿using System.Buffers;
+using Functions;
 
-namespace MathK
+namespace Calculator
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            Operators.WriteToHistory("Test", 1.0f);
-        }
-    }
-    
-    public static class Operators
-    {
-        public static float Pow(float number, int powerOf)
-        {
-            float result = number;
-            for (int i = 0; i < powerOf; i++)
-            {
-                result *= number;
-            }
-            return result;
-        }
-
-        public static float Sqrt(float number, int sqrtNum)
-        {
-            float result = number;
-            result *= 1.0f/sqrtNum;
-            return result;
-        }
-        public static void WriteToHistory(string operation, float result)
-        {
-            File.AppendAllText($"{Path.GetTempPath()}history.log", $"{operation} {result}");
+            Miscellaneous.WriteToHistory("test", 1);
         }
     }
 }
