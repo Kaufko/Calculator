be179ea68d5093c24869559e5cbd6bc6f099b6cd
Kaufko <foxyczytcz@gmail.com>
2024-03-04 17:13:05 +0100

    Added menu structure and multiple menus.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,14 +1,46 @@
-﻿using System.Buffers;
-using Functions;
+﻿using Functions;
+using MenuLib;
 
 namespace Calculator
 {
     public static class Program
     {
+        static List<Menu> mainMenu;
+        static List<Menu> calcMenu;
+        static List<Menu> wholeNumMenu;
+        static List<Menu> decNumMenu;
         public static void Main(string[] args)
         {
-            Console.WriteLine(Operators.Root(1000000, 5));
-            Console.WriteLine(Operators.Root(81, 4));
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            mainMenu = new List<Menu>
+            {
+                new Menu("Kalkulačka", () => MenuK.SelectSubMenu(calcMenu)),
+                new Menu("Ovládání", () => Miscellaneous.CwControls())
+            };
+
+            calcMenu = new List<Menu>
+            {
+                new Menu("Celá čísla", () => MenuK.SelectSubMenu(wholeNumMenu)),
+                new Menu("Desetiná čísla", () => MenuK.SelectSubMenu(decNumMenu)),
+                new Menu("Vlastní vzorec", () => Miscellaneous.Eval())
+            };
+
+            wholeNumMenu = new List<Menu>
+            {
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+                new Menu("", () => ),
+            };
+
+            MenuK.Start(mainMenu); //run last
         }
+
+        
+
     }
 }
